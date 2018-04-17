12d1d8bbd400849136387b703f1cb1903fb01397
Lulu <lulumutuli24@gmail.com>
2018-04-17 15:54:54 +0300

    views page updated

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,33 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template, request, redirect, url_for
 from . import main
+from ..request import get_sources,get_articles
+from ..models import News, Article
 
-# Views
 @main.route('/')
 def index():
-	title = 'Flask Base'
-	return render_template('index.html',title = title)
+	'''
+	View roote page function that return the index page and its data
+	'''
+
+	status_sources = get_sources('sources')
+
+	title='Home - Get the most up to date news on News One'
+	return render_template('index.html',title=title, status = status_sources)
+
+@main.route('/news/<news_id>')
+def news(news_id):
+	'''
+	Views news page function that returns news details page and its data
+	'''
+
+@main.route('/article/<id>')
+def article(id):
+	'''
+	View articles page function for all the articles for specific source
+	'''
+
+	articles_sources = get_articles(id)
+
+	
+
+	return render_template('news.html', articles = articles_sources)
