9807735a728c9ff0fc38eb9091219bbcc5fd3bdc
sami-mai <samirah.maison@gmail>
2018-04-17 17:21:26 +0300

    define test fuction as a manager command

--- manage.py
+++ manage.py
@@ -1,26 +1,22 @@
-from app import create_app,db
-from flask_script import Manager,Shell,Server
-from  flask_migrate import Migrate, MigrateCommand
+from app import create_app
+from flask_script import Manager, Server
 
-# Creating app instance
+# creating  app isinstance
 app = create_app('development')
 
 manager = Manager(app)
-manager.add_command('server',Server)
+manager.add_command('server', Server)
 
-migrate = Migrate(app,db)
-manager.add_command('db',MigrateCommand)
 
 @manager.command
 def test():
-    """Run the unit tests."""
+    '''
+    Run the unit tests
+    '''
+
     import unittest
     tests = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(tests)
-
-@manager.shell
-def make_shell_context():
-    return dict(app = app,db = db)
 
 
 if __name__ == '__main__':
