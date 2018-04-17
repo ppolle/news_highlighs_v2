42e34777a0073a21cd0690f105f7461c78866b9d
sami-mai <samirah.maison@gmail>
2018-04-17 17:17:44 +0300

    update news article class

--- app/models.py
+++ app/models.py
@@ -1,26 +1,27 @@
-class NewsSources:
+class NewsSource:
     '''
     NewsSources class to define news sources object
     '''
 
-    def __init__(self, id, name, description, url, category, country, language):
+    def __init__(self, id, name, description, url, category, language, country):
         self.id = id
         self.name = name
         self.description = description
         self.url = url
         self.category = category
-        self.country = country
         self.language = language
+        self.country = country
 
-class NewsArticles:
+
+class NewsArticle:
     '''
     NewsArticles class to define news articles object
     '''
-    def __init__(self, id, author, title, description, url, image, date):
-        self.id = id
+    def __init__(self, source, author, title, description, url, urlToImage, publishedAt):
+        self.source = source
         self.author = author
         self.title = title
         self.description = description
         self.url = url
-        self.image = image
-        self.date = date
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
